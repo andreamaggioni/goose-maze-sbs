8e8bba115a87784cd2a35904135ad3e777844071
Andrea Maggioni <andrea.maggioni@alten.it>
2019-05-09 22:59:26 +0200

    add maven nature

---
+++ src/main/java/com/maggio/game/goosemaze/GoosemazeApplication.java
@@ -0,0 +1,16 @@
+package com.maggio.game.goosemaze;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+
+@SpringBootApplication
+public class GoosemazeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoosemazeApplication.class, args);
+	}
+}
