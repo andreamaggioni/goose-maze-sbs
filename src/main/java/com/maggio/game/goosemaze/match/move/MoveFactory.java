46bc2cfcf1824ce5b93ee5c000fe284cc0a64813
Andrea Maggioni <andrea.maggioni@alten.it>
2019-05-13 23:12:47 +0200

    aggiunto rename della casella 0 a "Start"

--- src/main/java/com/maggio/game/goosemaze/match/move/MoveFactory.java
+++ src/main/java/com/maggio/game/goosemaze/match/move/MoveFactory.java
@@ -21,13 +21,15 @@ public class MoveFactory {
 
     @Value("${march.gooseSpaces}") private Set<Integer> gooseSpaces;
 
+    @Value("${match.startCellName:Start}") private String startCellName;
+
     public WinMove winMove(String player, int firstRoll, int secondRoll, int currentSpace){
-        String message = messageSource.getMessage("match.win", player, firstRoll, secondRoll, currentSpace, winCell);
+        String message = messageSource.getMessage("match.win", player, firstRoll, secondRoll, this.getCurrentSpace(currentSpace), winCell);
         return new WinMove(player, firstRoll, secondRoll, currentSpace, winCell, message);
     }
 
     public BridgeMove bridgeMove(String player, int firstRoll, int secondRoll, int currentSpace) {
-        String message = messageSource.getMessage("match.bridge", player, firstRoll, secondRoll, currentSpace, bridgeSpaceEnd);
+        String message = messageSource.getMessage("match.bridge", player, firstRoll, secondRoll, this.getCurrentSpace(currentSpace), bridgeSpaceEnd);
         return new BridgeMove(player, firstRoll, secondRoll, currentSpace, bridgeSpaceEnd, message);
     }
 
@@ -35,7 +37,7 @@ public class MoveFactory {
         int newSpace = firstRoll + secondRoll + currentSpace;
 
         StringBuilder sb = new StringBuilder();
-        sb.append(messageSource.getMessage("match.goose", player, firstRoll, secondRoll, currentSpace, newSpace)).append(" ");
+        sb.append(messageSource.getMessage("match.goose", player, firstRoll, secondRoll, this.getCurrentSpace(currentSpace), newSpace)).append(" ");
 
         while(gooseSpaces.contains(newSpace)){
             newSpace = newSpace + firstRoll + secondRoll;
@@ -48,13 +50,20 @@ public class MoveFactory {
 
     public Move move(String player, int firstRoll, int secondRoll, int currentSpace){
         int newSpace = firstRoll + secondRoll + currentSpace;
-        String message = messageSource.getMessage("match.move", player, firstRoll, secondRoll, currentSpace, newSpace);
+        String message = messageSource.getMessage("match.move", player, firstRoll, secondRoll, this.getCurrentSpace(currentSpace), newSpace);
         return new Move(player, firstRoll, secondRoll, currentSpace, newSpace, message);
     }
 
     public BounceMove bounceMove(String player, Integer firstRoll, Integer secondRoll, int currentSpace) {
         int bounceSpace = winCell - (currentSpace + firstRoll + secondRoll - winCell);
-        String message = messageSource.getMessage("match.bounce", player, firstRoll, secondRoll, currentSpace, winCell, bounceSpace);
+        String message = messageSource.getMessage("match.bounce", player, firstRoll, secondRoll, this.getCurrentSpace(currentSpace), winCell, bounceSpace);
         return new BounceMove(player, firstRoll, secondRoll, currentSpace, bounceSpace, message);
+    }
+
+    private Object getCurrentSpace(int currentSpace) {
+        if(currentSpace == 0){
+            return startCellName;
+        }
+        return currentSpace;
     }
 }
